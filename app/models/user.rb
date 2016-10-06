eef4d2a66c9faaf080ab964a05a3f2acb84ecfbd
willdunlop <william.dunlop.1993@gmail.com>
2016-10-06 20:36:01 +1100

    Profiles working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :profile
+
+  after_create :create_profile
+  
 end
