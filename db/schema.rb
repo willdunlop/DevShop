1ac6030ce0a9f17044dee4f401ad7322dd731d0b
willdunlop <william.dunlop.1993@gmail.com>
2016-10-11 22:46:58 +1100

    Migration to postgresql, More styling

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161010105529) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dev_corners", force: :cascade do |t|
     t.integer  "profile_id"
     t.text     "skills"
@@ -25,11 +28,11 @@ ActiveRecord::Schema.define(version: 20161010105529) do
     t.integer  "cached_votes_score", default: 0
     t.integer  "cached_votes_up",    default: 0
     t.integer  "cached_votes_down",  default: 0
-    t.index ["cached_votes_down"], name: "index_dev_corners_on_cached_votes_down"
-    t.index ["cached_votes_score"], name: "index_dev_corners_on_cached_votes_score"
-    t.index ["cached_votes_total"], name: "index_dev_corners_on_cached_votes_total"
-    t.index ["cached_votes_up"], name: "index_dev_corners_on_cached_votes_up"
-    t.index ["profile_id"], name: "index_dev_corners_on_profile_id"
+    t.index ["cached_votes_down"], name: "index_dev_corners_on_cached_votes_down", using: :btree
+    t.index ["cached_votes_score"], name: "index_dev_corners_on_cached_votes_score", using: :btree
+    t.index ["cached_votes_total"], name: "index_dev_corners_on_cached_votes_total", using: :btree
+    t.index ["cached_votes_up"], name: "index_dev_corners_on_cached_votes_up", using: :btree
+    t.index ["profile_id"], name: "index_dev_corners_on_profile_id", using: :btree
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -41,7 +44,7 @@ ActiveRecord::Schema.define(version: 20161010105529) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
-    t.index ["user_id"], name: "index_profiles_on_user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,8 +60,8 @@ ActiveRecord::Schema.define(version: 20161010105529) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "votes", force: :cascade do |t|
@@ -71,8 +74,10 @@ ActiveRecord::Schema.define(version: 20161010105529) do
     t.integer  "vote_weight"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
-    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
   end
 
+  add_foreign_key "dev_corners", "profiles"
+  add_foreign_key "profiles", "users"
 end
